5c694fa2dc410fd9a520e6550393384c873bcacb
Azhari Muhammad M <azhari.marzan@gmail.com>
2020-08-28 09:24:14 +0700

    + context api setup

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,16 +1,24 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import { useStateValue } from '../StateProvider';
 import SearchIcon from '@material-ui/icons/Search';
 import MicIcon from '@material-ui/icons/Mic';
 import { Button } from '@material-ui/core';
+import { actionTypes } from '../reducer';
 import './SearchBar.css';
 
 const SearchBar = ({ hideButtons = false }) => {
+    const [{}, dispatch] = useStateValue();
     const [input, setInput] = useState('');
     const history = useHistory();
 
     const search = (e) => {
         e.preventDefault();
+        dispatch({
+            type: actionTypes.SET_SEARCH_TERM,
+            term: input
+        });
+        
         history.push('/search');
     };
 
